dfced41b0a990caf28dace8fae3db7d553354e05
teeerapong2020 <teerapong.chue@gmail.com>
2025-06-26 18:27:46 +0700

    delete log

--- utlit/lib/connect_db.js
+++ utlit/lib/connect_db.js
@@ -5,10 +5,9 @@ configDotenv();
 
 const supabase  = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
-console.log("SUPABASE_URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
-console.log("SERVICE_ROLE_KEY:", process.env.SUPABASE_SERVICE_ROLE_KEY);
+
 
 
 export default supabase 
