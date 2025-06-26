d1597701b8c5c1e1f9b99d8c6fcd0781666dc3bd
teeerapong2020 <teerapong.chue@gmail.com>
2025-06-26 18:12:44 +0700

    log check connect superbase

--- utlit/lib/connect_db.js
+++ utlit/lib/connect_db.js
@@ -7,6 +7,8 @@ const supabase  = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY
 );
+console.log("SUPABASE_URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
+console.log("SERVICE_ROLE_KEY:", process.env.SUPABASE_SERVICE_ROLE_KEY);
 
 
 export default supabase 
