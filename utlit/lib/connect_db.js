41912b42238fdbc7a10cfa8095b46e283a4e0b17
teeerapong2020 <teerapong.chue@gmail.com>
2025-06-26 18:41:04 +0700

    Update connect_db.js

--- utlit/lib/connect_db.js
+++ utlit/lib/connect_db.js
@@ -5,9 +5,8 @@ configDotenv();
 
 const supabase  = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  process.env.SUPABASE_SERVICE_ROLE_KEY
 );
-
 
 
 export default supabase 
